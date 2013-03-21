be55c93af6460bc5ea04f86fb8a1d0cd589ef5a5
Simon Hafner <hafnersimon@gmail.com>
2013-03-21 00:11:40 -0500

    glorious tests

--- src/main/scala/Counter.scala
+++ src/main/scala/Counter.scala
@@ -1,23 +1,29 @@
-package reactormonk.counter
-import scala.collection.{GenMap,MapLike}
+package reactormonk
+import Numeric.Implicits._
 
-class Counter[A](counter: Map[A, Int]) extends Iterable[(A, Int)]{
-  def +(key: A): Counter[A] = this.change(key, 1)
-  def -(key: A): Counter[A] = this.change(key, -1)
-  def change(key: A, by: Int): Counter[A] = Counter((counter + (key -> {by.+(apply(key)):Int})))
-  def ++(other: Counter[A]): Counter[A] = other.foldLeft(this)({case (counter, (key, count)) => counter.change(key, count)})
-  def get(key: A): Option[Int] = counter.get(key)
-  def apply(key: A): Int = counter.getOrElse(key, 0)
-  def toMap(): Map[A, Int] = counter
-  override def iterator(): Iterator[Tuple2[A, Int]] = counter.iterator
-  override def toList(): List[Tuple2[A, Int]] = counter.toList
-  override def toSeq(): Seq[Tuple2[A, Int]] = counter.toSeq
-  def empty(): Counter[A] = Counter[A]()
-  override def size: Int = counter.size
+class Counter[A, B](counter: Map[A, B])(implicit num:Numeric[B]) {
+  def +(key: A): Counter[A, B] = this.change(key, num.fromInt(1))
+  def -(key: A): Counter[A, B] = this.change(key, num.fromInt(-1))
+  def change(key: A, by: B): Counter[A, B] = Counter((counter + (key -> {by.+(apply(key)):B})))
+  def ++(other: Counter[A, B]): Counter[A, B] = other.iterator.foldLeft(this)({case (counter, (key, count)) => counter.change(key, count)})
+  def get(key: A): Option[B] = counter.get(key)
+  def apply(key: A): B = counter.getOrElse(key, num.fromInt(0))
+  def toMap(): Map[A, B] = counter
+  def iterator(): Iterator[Tuple2[A, B]] = counter.iterator
+  def toList(): List[Tuple2[A, B]] = counter.toList
+  def toSeq(): Seq[Tuple2[A, B]] = counter.toSeq
+  def empty(): Counter[A, B] = Counter[A, B]()
+  def size: Int = counter.size
+  override def equals(other: Any): Boolean = other match {
+    case (other: Counter[A, B]) => counter.toMap.equals(other.toMap)
+    case _ => false
+  }
+  override def hashCode = counter.hashCode
+  override def toString: String = counter.toString.replaceFirst("Map", "Counter") // cheap but effective
 }
 
 object Counter {
-  def apply[A](): Counter[A] = apply(Map[A, Int]())
-  def apply[A](counter: Map[A, Int]): Counter[A] = { new Counter[A](counter) }
-  def apply[A](count: Seq[A]): Counter[A] = count.foldLeft(apply[A]())({case (counter, element) => counter + element})
+  def apply[A, B: Numeric](): Counter[A, B] = apply(Map[A, B]())
+  def apply[A, B: Numeric](counter: Map[A, B]): Counter[A, B] = { new Counter[A, B](counter) }
+  def apply[A](count: Iterable[A]): Counter[A, Int] = count.foldLeft(apply[A, Int]())({case (counter, element) => counter + element})
 }
---
+++ src/test/scala/CounterTest.scala
@@ -0,0 +1,55 @@
+import reactormonk.Counter
+import org.scalatest.FunSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class CounterSpec extends FunSpec with ShouldMatchers {
+  val simple = Counter(Map("foo" -> 1, "bar" -> 2))
+
+  it("should convert a map and back") {
+    simple.toMap should equal(Map("foo" -> 1, "bar" -> 2))
+  }
+  it("should add a key") {
+    (simple + "foo").toMap should equal(Map("foo" -> 2, "bar" -> 2))
+  }
+  it("should subtract a key (and keep it if 0)") {
+    (simple - "foo").toMap should equal(Map("foo" -> 0, "bar" -> 2))
+  }
+  it("should also allow full changing") {
+    (simple.change("foo", 2)).toMap should equal(Map("foo" -> 3, "bar" -> 2))
+  }
+  // TODO: Anyone got an idea how to test this?
+  // it("should disallow type changes") {
+  //   (simple.change("foo", 0.5))
+  // }
+  it("should also allow full changing for negative values") {
+    (simple.change("foo", -2)).toMap should equal(Map("foo" -> -1, "bar" -> 2))
+  }
+  it("should add together two counters") {
+    (simple ++ Counter(Map("bar" -> -1))).toMap should equal(Map("foo" -> 1, "bar" -> 1))
+  }
+  it("should return an Option containing the element") {
+    simple.get("foo") should equal (Some(1))
+  }
+  it("should return the element or 0") {
+    simple("foo") should equal(1)
+    simple("baz") should equal(0)
+  }
+  it("should return a list") {
+    simple.toList should equal(List(("foo", 1), ("bar", 2)))
+  }
+  it("should return a sequence") {
+    simple.toSeq should equal(Seq(("foo", 1), ("bar", 2)))
+  }
+  it("should test equality correctly") {
+    Counter(Map("foo" -> 1)) should equal(Counter(Map("foo" -> 1)))
+  }
+  it("should return an empty counter") {
+    simple.empty should equal(Counter(simple.toMap.empty))
+  }
+  it("should return the number of tokens") {
+    simple.size should equal(2)
+  }
+  it("should count the element of an Iterable when constructed that way") {
+    Counter(Seq("bar", "foo", "bar")) should equal(simple)
+  }
+}
